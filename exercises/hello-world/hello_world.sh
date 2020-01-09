61e5e9feab71a7316a121b26c83dfbdafb96cada
Aakriti <aakriti.1012@gmail.com>
2020-01-09 12:17:48 +0530

    Added two exercises: hello-world, reverse-string.

---
+++ exercises/hello-world/hello_world.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+function hello_world() {
+  var='Hello, World!'
+}
+
+var=''
+hello_world
+echo $var
+
---
+++ exercises/reverse-string/reverse_string.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "This script will reverse whatever you type."
+echo "Please input a string"
+read input
+echo $input | rev
