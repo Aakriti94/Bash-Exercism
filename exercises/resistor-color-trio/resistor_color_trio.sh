0216f2e7589328d98a6cdc471124aa9a1b56b7c8
Aakriti <aakriti.1012@gmail.com>
2020-01-25 21:48:41 +0530

    Added documentation to Resistor Color Trio
    Added documentation and made some changes in  the logic part of the code.

--- exercises/resistor-color-trio/resistor_color_trio.sh
+++ exercises/resistor-color-trio/resistor_color_trio.sh
@@ -9,43 +9,33 @@ function resistor_color_trio() {
   case "$1" in
     [B,b]lack )
     retval=0
-    zeros=""
       ;;
     [B,b]rown )
     retval=1
-    zeros=0
       ;;
     [R,r]ed )
     retval=2
-    zeros=00
       ;;
     [O,o]range )
     retval=3
-    zeros=000
       ;;
     [Y,y]ellow )
     retval=4
-    zeros=0000
       ;;
     [G,g]reen )
     retval=5
-    zeros=00000
       ;;
     [B,b]lue )
     retval=6
-    zeros=000000
       ;;
     [V,v]iolet )
     retval=7
-    zeros=0000000
       ;;
     [G,g]rey )
     retval=8
-    zeros=00000000
       ;;
     [W,w]hite )
     retval=9
-    zeros=000000000
       ;;
     *)
     echo "invalid color"
@@ -80,6 +70,9 @@ function conversion() {
 #resistor_color_trio "$2"
 #outputString="${outputString}${retval}"
 
+#resistor_color_trio "$3"
+#outputString="${outputString}${zeros}"
+
 resistor_color_trio "$1"
 outputString=$((retval*10))
 
@@ -87,6 +80,6 @@ resistor_color_trio "$2"
 outputString=$((outputString + retval))
 
 resistor_color_trio "$3"
-outputString="${outputString}${zeros}"
+outputString="$((outputString * 10**retval))"
 
 conversion
